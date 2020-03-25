5b5d6b8ae315ede44230fa3e5fca32dd0999b9f7
wbrink <will.brink55@gmail.com>
2020-03-25 14:58:55 -0700

    finished add employee, role, department

--- app.js
+++ app.js
@@ -35,14 +35,26 @@ async function startPrompt() {
     case "View All Employees By Department":
       viewEmployeesByDepartment();
       break;
-    case "Add Department":
-      addDepartment();
+    case "View All Employees By Manager":
+      viewEmployeesByManager();
+      break;
+    case "View Departments":
+      viewDepartments();
+      break;
+    case "View Roles":
+      viewRoles();
+      break;
+    case "Add Employee":
+      addEmployee();
       break;
     case "Add Role":
       addRole();
       break;
-    case "Add Employee":
-      addEmployee();
+    case "Add Department":
+      addDepartment();
+      break;
+    case "Remove Employee":
+      removeEmployee();
       break;
     case "Quit":
       // close the database connection
@@ -68,28 +80,94 @@ function viewAllEmployees() {
   });
 }
 
-// add department 
-async function addDepartment() {
-  let question = [
-    {
-      type: "input",
-      message:"Department Name: ",
-      name: "departmentName"
-    }
-  ]
-  const answer = await inquirer.prompt(question);
 
-  const sql = "INSERT INTO department SET ?";
-  const placeholder = {name: answer.departmentName};
-  connection.query(sql, placeholder, (err, res, fields) => {
-    if (err) {
-      console.log("\nError: " + err.message);
-      return;
-    }
-    console.log(`${answer.departmentName} added to departments`);
-    startPrompt(); // restart the prompt
+//  view employees by department
+async function viewEmployeesByDepartment() {
+  let answer;
+  let departments;
+  try {
+    departments = await querySync(connection, "SELECT name FROM department");
+    departments = departments.map(elem => elem.name);
+    answer = await inquirer.prompt(
+      {
+        type: "list",
+        message: "Department Name: ",
+        name: "department",
+        choices: departments
+      }
+    )
+  } catch(err) {
+    throw err;
+  }
+
+  // query the database
+  const sql = "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name as department, role.salary, CONCAT(e.first_name, ' ', e.last_name) as manager"
+            + " FROM employee" +
+            " inner join role ON (employee.role_id = role.id)" + 
+            " inner join department on role.department_id = department.id" +
+            " left join employee as e on employee.manager_id = e.id" +
+            " WHERE department.name = ?";
+  connection.query(sql, [answer.department], (err, res, fields) => {
+    if (err) throw err;
+    console.table(res); // takes in array of objects with same properties
+    startPrompt();
+  });
+}
+
+//  view employees by manager
+async function viewEmployeesByManager() {
+  let answer;
+  let managers;
+  try {
+    const managersQuery = await querySync(connection, "SELECT e.id, CONCAT(e.first_name, ' ', e.last_name) as manager FROM employee inner join employee as e on employee.manager_id = e.id GROUP BY manager;");
+    managers = managersQuery.map(elem => elem.manager);
+    answer = await inquirer.prompt(
+      {
+        type: "list",
+        message: "Department Name: ",
+        name: "manager",
+        choices: managers
+      }
+    )
+  } catch(err) {
+    throw err;
+  }
+
+  //console.log(managers);
+
+  // query the database
+  const sql = "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name as department, role.salary, CONCAT(e.first_name, ' ', e.last_name) as manager"
+            + " FROM employee" +
+            " inner join role ON (employee.role_id = role.id)" + 
+            " inner join department on role.department_id = department.id" +
+            " left join employee as e on employee.manager_id = e.id" +
+            " WHERE CONCAT(e.first_name, ' ', e.last_name) = ?";
+  connection.query(sql, [answer.manager], (err, res, fields) => {
+    if (err) throw err;
+    console.table(res); // takes in array of objects with same properties
+    startPrompt();
+  });
+}
+
+
+function viewDepartments() {
+  const sql = "SELECT name as 'Department' FROM department"
+  connection.query(sql, (error, result) => {
+    if (error) throw error;
+    console.table(result)
+    startPrompt();
   })
 }
+
+function viewRoles() {
+  const sql = "SELECT title as role, salary FROM role";
+  connection.query(sql, (error, result) => {
+    if (error) throw error;
+    console.table(result);
+    startPrompt();
+  })
+}
+
 
 // add employee
 async function addEmployee() {
@@ -148,47 +226,78 @@ async function addEmployee() {
       console.log("\nError: " + err.message);
       return;
     }
-    console.log(`${answer.departmentName} added to departments`);
+    console.log(`${answer.firstName} ${answer.lastName} added to Employees`);
     startPrompt(); // restart the prompt
   })
 }
 
-async function viewEmployeesByDepartment() {
-  // const departments = await querySync(connection, "SELECT name FROM department");
-  // console.log(departments);
+
+// adds role to database
+async function addRole() {
+  let departmentsQuery;
+  let departmentsArray;
   let answer;
-  let departments;
   try {
-    departments = await querySync(connection, "SELECT name FROM department");
-    departments = departments.map(elem => elem.name);
-    answer = await inquirer.prompt(
+    departmentsQuery = await querySync(connection, "SELECT id, name FROM department", []);
+    departmentsArray = departmentsQuery.map(elem => elem.name); // array of department names
+    console.log(departmentsQuery);
+    answer = await inquirer.prompt([
+      {
+        type: "input",
+        message: "Role: ",
+        name: "role"
+      },
+      {
+        type: "input",
+        message: "Salary: ",
+        name: "salary",
+        validate: function(value) {
+          if (isNaN(parseInt(value))) return "Please input a number";
+          return true;
+        }
+      },
       {
         type: "list",
-        message: "Department Name: ",
-        name: "department",
-        choices: departments
+        message: "What Department is this role under: ",
+        choices: departmentsArray,
+        name: "department"
       }
-    )
+    ]);
   } catch(err) {
     throw err;
   }
 
-  const sql = "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name as department, role.salary, CONCAT(e.first_name, ' ', e.last_name) as manager"
-            + " FROM employee" +
-            " inner join role ON (employee.role_id = role.id)" + 
-            " inner join department on role.department_id = department.id" +
-            " left join employee as e on employee.manager_id = e.id" +
-            " WHERE department.name = ?";
-  connection.query(sql, [answer.department], (err, res, fields) => {
-    if (err) throw err;
-    console.table(res); // takes in array of objects with same properties
+  const departmentID = departmentsQuery.filter(elem => elem.name === answer.department)[0].id;
+  const sql = "INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)" 
+  connection.query(sql, [answer.role, answer.salary, departmentID], (error, result) => {
+    if(error) throw error;
+    console.log(`${answer.role} added to roles`);
     startPrompt();
-  });
+  })
 }
 
+// add department 
+async function addDepartment() {
+  let question = [
+    {
+      type: "input",
+      message:"Department Name: ",
+      name: "departmentName"
+    }
+  ]
+  const answer = await inquirer.prompt(question);
 
-
-
+  const sql = "INSERT INTO department SET ?";
+  const placeholder = {name: answer.departmentName};
+  connection.query(sql, placeholder, (err, res, fields) => {
+    if (err) {
+      console.log("\nError: " + err.message);
+      return;
+    }
+    console.log(`${answer.departmentName} added to departments`);
+    startPrompt(); // restart the prompt
+  })
+}
 
 // function returns promise with results
 function querySync(connection, sql, args) {
@@ -199,3 +308,5 @@ function querySync(connection, sql, args) {
     });
   })
 }
+
+
